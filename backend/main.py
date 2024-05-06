2d67dc158beb23c5ae705a439b9c144ec9acdf53
Carlos Forgiony <carlos.forgiony@gmail.com>
2024-05-06 04:33:27 -0500

    *Refactored recipe_bot *Created api POST endpoint to interact with the bot sending messages from the frontend *Added backend testing workflow for CI/CD

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,35 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from typing import List
 import os
-from identify import model_predict
-from werkzeug.utils import secure_filename
+
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from werkzeug.utils import secure_filename
+
+from identify import model_predict
+from recipe_bot import Chatbot, Vectorstore
 
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
 app = FastAPI()
+vectorstore = Vectorstore()
+chatbot = Chatbot(vectorstore)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Reemplaza esto con el origen de tu aplicación Angular
+    allow_origins=[
+        "http://localhost:4200"
+    ],  # Reemplaza esto con el origen de tu aplicación Angular
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Método HTTP permitido en las solicitudes CORS
+    allow_methods=[
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+    ],  # Método HTTP permitido en las solicitudes CORS
     allow_headers=["Authorization", "Content-Type"],
 )
+
 
 class Prediction(BaseModel):
     filename: str
@@ -35,7 +45,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/")
+@app.post("/api/classification")
 async def upload_file(file: UploadFile = File(...)):
     if not file:
         raise HTTPException(status_code=400, detail="No file provided")
@@ -52,6 +62,17 @@ async def upload_file(file: UploadFile = File(...)):
     print(prediction)
 
     return prediction
+
+
+@app.post("/api/chat")
+async def chat_endpoint(message: str):
+    # Process the incoming message with the chatbot
+    response_text = chatbot.process_message(message)
+
+    if response_text is None:
+        raise HTTPException(status_code=500, detail="Chatbot error")
+
+    return {"response": response_text}
 
 
 if __name__ == "__main__":
